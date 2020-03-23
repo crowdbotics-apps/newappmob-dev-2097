cf2db2f5ca8686768d751df18a0805c37c5e971d
Crowdbotics <team@crowdbotics.com>
2020-03-23 07:57:31 +0000

    Update models

---
+++ backend/user/models.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db import models
+
+
+class Users(models.Model):
+    "Generated Model"
+    hghg = models.BigIntegerField()
+
+
+# Create your models here.
