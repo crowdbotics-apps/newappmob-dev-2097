bbc8390b147286ee305b1b0623e085fae69969e5
Crowdbotics <team@crowdbotics.com>
2020-03-23 08:16:03 +0000

    Update models

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
-from .models import HomePage, CustomText, Test
+from .models import HomePage, Testing, CustomText, Test
 
 
 def home(request):
